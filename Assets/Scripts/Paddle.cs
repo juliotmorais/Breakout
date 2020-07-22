0db21642ce839161fcde930cb719886c4d5ea357
Julio <juliotmorais@gmail.com>
2020-07-22 16:41:40 +0900

    added lose screen, losecollider+script, paddle now moves on x axis with mouse position

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]float screenWidthInUnits=16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mosPosX= (Input.mousePosition.x/Screen.width*screenWidthInUnits);
+        Vector2 paddlePosX = new Vector2(mosPosX,0.5f);
+        transform.position = paddlePosX;
+    }
+}
