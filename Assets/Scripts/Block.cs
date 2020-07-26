a66766a2161b016e7b03d94283be3fabdd6a7dd4
juliotmorais <juliotmorais@gmail.com>
2020-07-26 23:42:57 +0900

    added score display

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -7,16 +7,19 @@ public class Block : MonoBehaviour
     [SerializeField] AudioClip breakSound;
 
     Level level;
+    GameStatus gamestatus;
 
     private void Start()
     {
         level = FindObjectOfType<Level>();
         level.CountBreakableBlocks();
+        gamestatus = FindObjectOfType<GameStatus>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
         level.MinusBeakableBlocks();
+        gamestatus.UpdateScore();
     }
 }
---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameStatus : MonoBehaviour
+{
+    [Range(0.1f,10f)][SerializeField] float gameSpeed = 1f;
+    [SerializeField] int score = 0;
+    [SerializeField] int pointsPerBlock = 100;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+    public void UpdateScore()
+    {
+        score=score+pointsPerBlock;
+        scoreText.text = score.ToString();
+    }
+}
